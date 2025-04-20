164bb27236cdc95aaa8a5c945fb1829db62deb4b
saravert <sara.s.vertanen@gmail.com>
2025-04-20 16:58:56 +0300

    Content using props

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -6,8 +6,14 @@ const Header = (props) => {
  )
 }
 
-const Content = () => {
-
+const Content = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+)
 }
 
 const Total = () => {
@@ -25,16 +31,11 @@ const App = () => {
 
   return (
     <div>
-       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />      
+      <Content part={part2} exercises={exercises2}/> 
+      <Content part={part3} exercises={exercises3}/>
+
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
