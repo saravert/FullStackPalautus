99aa528d92bec7080106597a68505baae5c9af86
saravert <sara.s.vertanen@gmail.com>
2025-04-27 15:19:18 +0300

    step3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -7,6 +7,23 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+ const sum = props.good + props.bad + props.neutral
+ const average = (props.good*1 + props.bad*-1 + props.neutral*0) / sum
+ const positive = ((props.good / sum)*100)
+  
+ return (
+  <div>
+    <div>good: {props.good}</div>
+      <div>neutral: {props.neutral}</div>
+      <div>bad: {props.bad}</div>
+      <div>all: {sum}</div>
+      <div>average: {average}</div>
+      <div>positive: {positive} %</div>
+  </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -21,12 +38,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text = "neutral"/>
       <Button onClick={() => setBad(bad + 1)} text = "bad"/>
       <Header header={statistics}/>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {good + bad + neutral}</div>
-      <div>average: {(good*1 + bad*-1 + neutral*0)/(good + bad + neutral)}</div>
-      <div>positive: {(good/(good + bad + neutral))*100} %</div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
