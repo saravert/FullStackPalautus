df95618cbd36c13700b75537420b066a8f624595
saravert <sara.s.vertanen@gmail.com>
2025-05-03 17:36:33 +0300

    step1 completed

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -6,19 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName}/>
         </div>
         <div>
           <button type="submit">add</button>
+          <div>debug: {newName}</div>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+      {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
     </div>
   )
 
