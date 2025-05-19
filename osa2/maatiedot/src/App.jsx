f0198841cf41f842c52770d102c16aa62af0281e
saravert <sara.s.vertanen@gmail.com>
2025-05-19 12:20:47 +0300

    shows all countries

--- osa2/maatiedot/src/App.jsx
+++ osa2/maatiedot/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const App = () => {
 
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState ('')
+  const [search, setSearch] = useState([])
+
+  useEffect(() =>{
+    console.log('testiä')
+    if(countries){
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        setCountries(response.data)
+      })
+    }
+  }, [])
+
+  const handleChange = (event) => {
+    setSearch(event.target.search)
+    console.log('moikkuu')
+  }
+
+
   return (
     <div>
-      <h2>Add a new</h2>
+      <form>
+        find countries: <input value={search} onChange={handleChange}/>
+      </form>
+      <p>Too many matches, specify another filter</p>
+      <ul>
+        {countries.map(country => (
+          <li key={country.cca3}>{country.name.common}</li>
+        ))}
+      </ul>
     </div>
   )
 
