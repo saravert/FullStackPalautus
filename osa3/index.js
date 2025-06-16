50bb6dffcbcab0d31c12b695a77fc41e95ebc72a
saravert <sara.s.vertanen@gmail.com>
2025-06-16 12:35:07 +0300

    step6 completed

--- osa3/index.js
+++ osa3/index.js
@@ -65,9 +65,14 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'name missing',
+      error: 'name or number missing',
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
     })
   }
 
