ce29f96874e8e2f7d3bc9266fedd7cd0ac8c1622
saravert <sara.s.vertanen@gmail.com>
2025-06-16 11:26:28 +0300

    Step1 completed

--- osa3/index.js
+++ osa3/index.js
@@ -1,10 +1,40 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+let persons = [
+  {
+    id: '1',
+    content: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    content: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    content: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+    {
+    id: '4',
+    content: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  }
+]
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Phonebook in the api/persons')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
