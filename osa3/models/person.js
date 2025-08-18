a56c134630b5d494d069c94f8c690d7fa693d278
saravert <sara.s.vertanen@gmail.com>
2025-08-18 17:25:43 +0300

    step 20 complete

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -25,7 +25,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
 
   },
 })
