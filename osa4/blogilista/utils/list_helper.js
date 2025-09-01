f0127ceffd48526e8db42ed96f8f35ae1b6cede8
saravert <sara.s.vertanen@gmail.com>
2025-09-01 16:37:37 +0300

    step 5 complete

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,4 @@
+const lodash = require('lodash')
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
@@ -6,11 +7,28 @@ const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes
   }
-
   return blogs.reduce(reducer, 0) 
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0){
+    return null
+  }
   
+  let mostLiked = blogs[0]
+  for (let i = 0; i < blogs.length; i++){
+    if(mostLiked.likes < blogs[i].likes){
+      mostLiked = blogs[i]
+    }
+  }
+  return mostLiked
+}
+
+const mostBlogs = (blogs) => {
+  //if (blogs.length === 0) return null
+
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
