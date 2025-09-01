658272c7ccf401bcec94a8f720a0e98d9fa2f21a
saravert <sara.s.vertanen@gmail.com>
2025-09-01 10:37:56 +0300

    step 2 complete

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,18 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-
-const app = express()
+const app = require('./app') // varsinainen Express-sovellus
 const config = require('./utils/config')
-const Blog = require('./models/blog')
-const blogRouter = require('./controllers/blogs')
-
-mongoose.connect(config.MONGODB_URI)
-
-app.use(express.json())
-
-app.use('/api/blogs', blogRouter)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
