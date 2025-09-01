5ced159da66e6a30680977c79f213c372e7531ea
saravert <sara.s.vertanen@gmail.com>
2025-09-01 10:10:55 +0300

    Routet omaan tiedostoonsa

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -5,24 +5,13 @@ const mongoose = require('mongoose')
 const app = express()
 const config = require('./utils/config')
 const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
