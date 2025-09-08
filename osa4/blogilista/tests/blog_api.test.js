1cc7bd4ddd6d574e68d518f25609b7e2099363f0
saravert <sara.s.vertanen@gmail.com>
2025-09-08 17:36:59 +0300

    step 9 complete

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -41,6 +41,15 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+test('check if unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+  blogs.forEach((blog) => {
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
