956357924293b695326b7575863548dc95bdf595
saravert <sara.s.vertanen@gmail.com>
2025-09-26 17:53:28 +0300

    step 2 complete

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -15,11 +15,23 @@ const App = () => {
     )  
   }, [])
 
+    useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
     const handleLogin = async event => {
     event.preventDefault()
     console.log('logging in with', username, password)
         try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
       setUser(user)
       setUsername('')
       setPassword('')
@@ -29,6 +41,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedNoteappUser')
+    setUser(null)
   }
 
  if (user === null) {
@@ -66,7 +83,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>
-        {user.name} logged in
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
