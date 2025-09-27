b0b2a8b558b897b5fa6f498dd996eabc1b73c727
saravert <sara.s.vertanen@gmail.com>
2025-09-27 17:22:26 +0300

    step 10 complete

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -117,7 +117,7 @@ const App = () => {
       <Togglable buttonLabel="create new blog" ref={blogFormRef}>
         <BlogForm addBlog={addBlog} />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlogInState={updateBlogInState} />
       )}
     </div>
