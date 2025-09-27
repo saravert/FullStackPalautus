54a71449d9f5a2c2e72608358279234960b2e1a6
saravert <sara.s.vertanen@gmail.com>
2025-09-27 16:55:05 +0300

    step 7 complete

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [viewing, setViewing] = useState(false)
+
+    const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleView = () => {
+    setViewing(!viewing)
+  }
+
+  return (
+    <div style={blogStyle} className="blog">
+      {blog.title} {blog.author}
+      <button onClick={toggleView}>{viewing ? 'hide' : 'view'}</button>
+      {viewing && (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes} likes
+          <button onClick={() => console.log('liking', blog.id)}>like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
